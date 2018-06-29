238a0ab321ae58b31daba3e81a9c5a3380b529e6
Rick Cappe <blend2cap@gmail.com>
2018-06-29 09:21:29 +0200

    Add project files.

---
+++ DB2_DBA5/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace DB2_DBA5
+{
+    class Program
+    {
+        static int cont = 0;
+        static void Main(string[] args)
+        {
+
+            const string dba2_path = @"C:\Users\blend\Desktop\Programming\DBA2_DBA5\DBA2.txt";
+            const string dba5_path = @"C:\Users\blend\Desktop\Programming\DBA2_DBA5\DBA5.txt";
+            const string report_path = @"C:\Users\blend\Desktop\Programming\DBA2_DBA5\report.txt";
+
+            if (File.Exists(report_path))
+            {
+                File.Delete(report_path); 
+            }
+            string[] dba2_allLines = File.ReadAllLines(dba2_path);
+            string[] dba5_allLines = File.ReadAllLines(dba5_path);
+
+            List<Record> dba2_list = new List<Record>();
+            List<Record> dba5_list = new List<Record>();
+
+            
+            for (int i = 0; i < dba2_allLines.Length; i++)
+            {
+                dba2_list.Add(Format_DB(dba2_allLines[i], i));
+            }
+            
+            for (int i = 0; i < dba5_allLines.Length; i++)
+            {
+                dba5_list.Add(Format_DB(dba5_allLines[i], i));
+            }
+            var notPresentInDB5 = dba2_list.Except(dba5_list, new RecordComparer());
+            foreach (var item in notPresentInDB5)
+            {
+                //Console.WriteLine(item.one);
+                var formattedString = string.Format("{0,-40} {1,40}", item.one, item.row)+Environment.NewLine;
+                File.AppendAllText(report_path, formattedString);
+                cont++;
+            }
+            File.AppendAllText(report_path, Environment.NewLine+"Totale: "+cont.ToString());
+            Console.WriteLine("Cont: {0}", cont);
+            Console.ReadKey();
+            System.Diagnostics.Process.Start(report_path);
+        }
+
+
+        private static Record Format_DB(string record, int row)
+        {
+            string[] splitted = Split(record);
+            if (!splitted[0].Contains("#"))
+            {
+                var s = splitted;
+                Record rec = new Record(s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7], row);
+
+                if (rec.one.Equals(String.Empty)) rec.one = "vuoto";
+                return rec;
+            }
+            return null;
+        }
+
+        private static string[] Split(string record)
+        {
+            char comma = ',';
+            var line = Regex.Replace(record, @"[^u0000-u007F\s.+-]", "#"); //replace bad chars with #
+            line = Regex.Replace(line, @"[a-z]", "#");
+            line = Regex.Replace(line, @"\s+", ","); //insert commas in whitespaces
+            string[] splitted = line.Split(comma);
+            return splitted;
+        }
+
+    }
+}
---
+++ DB2_DBA5/Record.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DB2_DBA5
+{
+    class Record
+    {
+
+        public string one,
+        two,
+            three,
+            four,
+            five,
+            six,
+            seven,
+            eight;
+        public int row;
+
+        public Record(string one, string two, string three, string four, string five, string six, string seven, string eight, int row)
+        {
+            this.one = one;
+            this.two = two;
+            this.three = three;
+            this.four = four;
+            this.five = five;
+            this.six = six;
+            this.seven = seven;
+            this.eight = eight;
+            this.row = row;
+        }
+       
+    }
+
+    class RecordComparer : IEqualityComparer<Record>
+    {
+
+        public bool Equals(Record x, Record y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (x is null) return false;
+            if (y is null) return false;
+            return x.one == y.one && x.two == y.two && x.three == y.three && x.four == y.four && x.five == y.five &&
+                x.six == y.six && x.seven == y.seven && x.eight == y.eight;
+        }
+
+        public int GetHashCode(Record record)
+        {
+
+            if (record is null) return 0;
+            unchecked
+            {
+                int preComputeHash(int hashedRecord)
+                {
+                    var hashCode = 13;
+                    hashCode = (hashCode * 397) ^ hashedRecord;
+                    return hashCode;
+                }
+                int hash_one = record.one == null ? 0 : preComputeHash(record.one.GetHashCode());
+                int hash_two = record.two == null ? 0 : preComputeHash(record.two.GetHashCode());
+                int hash_three = record.three == null ? 0 : preComputeHash(record.three.GetHashCode());
+                int hash_four = record.four == null ? 0 : preComputeHash(record.four.GetHashCode());
+                int hash_five = record.five == null ? 0 : preComputeHash(record.five.GetHashCode());
+                int hash_six = record.six == null ? 0 : preComputeHash(record.six.GetHashCode());
+                int hash_seven = record.seven == null ? 0 : preComputeHash(record.seven.GetHashCode());
+                int hash_eight = record.eight == null ? 0 : preComputeHash(record.eight.GetHashCode());
+                return hash_one ^ hash_two ^ hash_three ^ hash_four ^ hash_five ^ hash_six ^ hash_seven ^ hash_eight;
+            }
+        }
+    }
+}
